b7ac28ab2b46e8b5e2e26eca61f05a7d661d855c
Varun Hegde <varunhegde2k@gmail.com>
2021-02-28 23:36:59 +0530

    Login Routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
 const { databaseVersion } = require('../dbConnection');
 const User = require('../models/User');
 const {hashPassword,matchPassword} = require('../utils/password')
+const {sign,decode} = require('../utils/jwt')
+
 
 module.exports.createUser = async (req,res) => {
-
     try{
         if(!req.body.user.username) throw new Error("Username is Required")
         if(!req.body.user.email) throw new Error("Email is Required")
@@ -23,10 +24,42 @@ module.exports.createUser = async (req,res) => {
         if(user){
             if(user.dataValues.password)
                 delete user.dataValues.password
-            
+            user.dataValues.token = await sign(user)
+            user.dataValues.bio = null
+            user.dataValues.image = null
             res.status(201).json({user})
         }    
     }catch (e){
         res.status(422).json({errors: { body: [ 'Could not create user ', e.message ] }})
     }   
+}
+
+module.exports.loginUser = async (req,res) => {
+    try{
+        if(!req.body.user.email) throw new Error('Email is Required')
+        if(!req.body.user.password) throw new Error('Password is Required')
+
+        const user = await User.findByPk(req.body.user.email)
+
+        if(!user){
+            res.status(401)
+            throw new Error('No User with this email id')
+        }
+        
+        //Check if password matches
+        const passwordMatch = await matchPassword(user.password,req.body.user.password)
+
+        if(!passwordMatch){
+            res.status(401)
+            throw new Error('Invalid password or email id')
+        }
+            
+        delete user.dataValues.password
+        user.dataValues.token = await sign({email: user.dataValues.email,username:user.dataValues.password})
+
+        res.status(200).json({user})
+    }catch(e){
+        const status = res.statusCode ? res.statusCode : 500
+        res.status(status).json({errors: { body: [ 'Could not create user ', e.message ] }})
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const { Router } = require('express')
 const express = require('express')
 const asyncHandler = require('express-async-handler')
 const router = express.Router()
-const {createUser} = require('../controllers/users')
+const UserController = require('../controllers/users')
 
-router.post('/users',createUser)
-
+router.post('/users',UserController.createUser)
+router.post('/users/login',UserController.loginUser)
 
 
 
