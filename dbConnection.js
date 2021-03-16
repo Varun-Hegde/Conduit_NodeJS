27b5037f9eb05ce22f6e5f5b6d773e53cdfbdc4e
Varun Hegde <varunhegde2k@gmail.com>
2021-03-16 22:41:25 +0530

    Changed database

--- dbConnection.js
+++ dbConnection.js
@@ -9,11 +9,24 @@ const {Sequelize} = require('sequelize')
 
 
 //AMAZON RDS CONNECTION
-const sequelize = new Sequelize('conduit1',process.env.USER_NAME,process.env.PASSWORD,{
+/* const sequelize = new Sequelize('conduit1',process.env.USER_NAME,process.env.PASSWORD,{
     dialect: 'mysql',
     host:process.env.DB_HOST,
     logging: false,
     port: 3306
+});
+ */
+const sequelize = new Sequelize('d6rk5ijgmvcf6q',process.env.USER_NAME,process.env.PASSWORD,{
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    logging: false,
+    port: 5432,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    }
 });
 
 const checkConnection =async () => {
