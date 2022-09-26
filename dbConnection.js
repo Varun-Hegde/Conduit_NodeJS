a2b8a2fe5e4366a6403016dc5c6c01400e3c761f
Marcio Flavio <mflaviof1995@gmail.com>
2022-09-26 10:59:42 -0300

    TS initial config

--- dbConnection.js
+++ dbConnection.js
@@ -1,43 +1,45 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
 
 //LOCAL CONNECTION
+
 /* const sequelize = new Sequelize('conduit','root','password',{
+
     dialect: 'mysql',
+
     host:'localhost',
+
     logging: false
+
 }); */
 
-
 //AMAZON RDS CONNECTION
+
 /* const sequelize = new Sequelize('conduit1',process.env.USER_NAME,process.env.PASSWORD,{
+
     dialect: 'mysql',
+
     host:process.env.DB_HOST,
+
     logging: false,
+
     port: 3306
+
 });
+
  */
-const sequelize = new Sequelize('d6rk5ijgmvcf6q',process.env.USER_NAME,process.env.PASSWORD,{
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    logging: false,
-    port: 5432,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-        }
-    }
-});
 
-const checkConnection =async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+const sequelize = new Sequelize("sqlite::memory:");
 
-checkConnection()
+const checkConnection = async () => {
+  try {
+    await sequelize.authenticate();
 
-module.exports = sequelize
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+checkConnection();
+
+module.exports = sequelize;
